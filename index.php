2fc76e77f474730c0152c3d0b33df7fd6fa070c4
Josue-Dominguez <jdominguez10@ucol.mx>
2022-05-30 09:35:02 -0500

    Agrege tarjetas con el texto de los sensores

--- index.php
+++ index.php
@@ -4,27 +4,66 @@
     session_start();
     if(isset($_SESSION['usuario'])){
 ?>
-
-<div class="container my-4">
-    
     <div class="card mb-4">
     <div class="text-center fw-bold">
         <h1>Walker Up</h1>
     </div>
-        <div class="row">
-            <div class="col mb-3 mx-3 my-3 card">
-               <p style="text-align: justify;">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex doloribus harum vitae, perspiciatis iste pariatur fugiat ad deserunt ut tempora quos, maiores et? Mollitia corporis dolorem dolorum commodi atque cumque, nisi quo fugit tempora, magni necessitatibus distinctio qui ipsa animi minima provident aperiam? Recusandae debitis expedita maxime adipisci. Harum atque aperiam, earum pariatur cum, labore consectetur maxime inventore delectus reprehenderit error amet voluptatem distinctio alias obcaecati eos rem dolore mollitia, praesentium non autem eum voluptatum. Illum ipsa ex, ratione expedita molestiae dolor natus voluptatum sequi, minima nisi ab, voluptatibus dicta dolorum corporis pariatur quam veritatis dolorem! Doloremque, explicabo. Illum sed perspiciatis libero. Cumque quae a deserunt illo voluptate fugit deleniti sint. Sed, reprehenderit. Non dignissimos voluptatibus deserunt explicabo at ipsum commodi nisi?
-               </p>
+
+    <div class="row mx-5">
+        <div class="container my-4">
+            <div class="card">
+            <div class="card-header" style="width: 10rem;">
+                <p class="text-danger">ADVERTENCIA!</p>
             </div>
-           
+            <div class="alert alert-danger" role="alert">
+                SE HA REGISTRADO UNA ACCIDENTE <a href="#" class="alert-link">ATENDER EMERGENCIA</a>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Boton de panico</h5>
+                <p class="card-text">Se mostrara informacion cuando se haya presionado el boton de emergencia</p>
+                <a href="#" class="btn btn-danger">Ver accidentes</a>
+            </div>
         </div>
-        <div class="row">
-            <div class="col mb-3 mx-3 my-3 card">
-               <p style="text-align: justify;">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex doloribus harum vitae, perspiciatis iste pariatur fugiat ad deserunt ut tempora quos, maiores et? Mollitia corporis dolorem dolorum commodi atque cumque, nisi quo fugit tempora, magni necessitatibus distinctio qui ipsa animi minima provident aperiam? Recusandae debitis expedita maxime adipisci. Harum atque aperiam, earum pariatur cum, labore consectetur maxime inventore delectus reprehenderit error amet voluptatem distinctio alias obcaecati eos rem dolore mollitia, praesentium non autem eum voluptatum. Illum ipsa ex, ratione expedita molestiae dolor natus voluptatum sequi, minima nisi ab, voluptatibus dicta dolorum corporis pariatur quam veritatis dolorem! Doloremque, explicabo. Illum sed perspiciatis libero. Cumque quae a deserunt illo voluptate fugit deleniti sint. Sed, reprehenderit. Non dignissimos voluptatibus deserunt explicabo at ipsum commodi nisi?
-               </p>
+    </div>
+    <div class="col ml5">
+        <div class="card" style="width: 20rem;">
+            <img src="resources/images/iconos svg/sensor-ultrasonico-explicacion.png" class="card-img-top" alt="logo">
+            <div class="card-body">
+                <h5 class="card-title">Sensor Ultrasonico</h5>
+                <p class="card-text">Aqui se muestra las distancias que se han registrado en la andadera</p>
+                <a href="#" class="btn btn-secondary">Ver detalles</a>
             </div>
+        </div> 
+    </div>
+    <div class="col ml5">
+        <div class="card" style="width: 15rem;">
+                <img src="resources/images/iconos svg/acele.jpg" class="card-img-top" alt="logotype">
+                 <div class="card-body">
+                    <h5 class="card-title">Registros de acelereación</h5>
+                    <p class="card-text">Vea si se tienen registros del acelerometro con una frecuencia acelerada</p>
+                    <a href="#" class="btn btn-primary">Ver registros</a>
+                 </div> 
+        </div>
+    </div>
+    <div class="col ml5">
+        <div class="card" style="width: 15rem;">
+                <img src="resources/images/iconos svg/o no.jpg" class="card-img-top" alt="logotype">
+                <div class="card-body">
+                    <h5 class="card-title">Registros de accidentes registrados en la andadera</h5>
+                    <p class="card-text">Vea si se tienen registros de una frecuencia acelerada</p>
+                    <a href="#" class="btn btn-warning">Atender</a>
+                </div>
+         </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">¿Que hacer en caso de una emergencia?</h5>
+            <h6 class="cfard-subtitle mb-2 text-muted">No dudes en llamar a algun servivicio de emergencia!</h6>
+            <p class="card-text">A continuacion te dejaremos los numeros de servicios de emergencia</p>
+            <p class="card-text">Numero de la cruz roja en Manzanillo :</p><p class="text-primary"> 314 336 5770 </p>
+        </div>
+    </div>
+
             <?php // progres bar--------------------------------------?>
            
             <div class="col mb-3 mx-3 my-3 card align-content-center">
