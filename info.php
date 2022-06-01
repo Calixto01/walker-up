e423e0e740ae49e55675a3be120a4705e456f570
WilliamRVazquez <wvazquez0@ucol.mx>
2022-06-01 12:33:07 -0500

    Se aplicaron cambios de diseño, no se alteró la funcionalidad

--- info.php
+++ info.php
@@ -10,7 +10,7 @@
 ?>
                              
 
-<div class="container card my-2">
+<div class="container">
     <h1 class="text-center"><i class="bi bi-info-circle-fill"></i>Information</h1>
     <table class="table table-hover table-responsive">
         <thead class="text-center">
@@ -21,29 +21,29 @@
         </thead>
         <tbody class="text-center">
             
-
+            
             <?php while($data=$datos->fetch(PDO::FETCH_ASSOC)){ ?>
-            <tr>
-                <td><h5><i class="bi bi-qr-code"></i> Id De La Andadera:</h5></td>
-                <td><?php echo $data['id_walker'];?></td>
-            </tr>
-            <tr>
-                <td><h5><i class="bi bi-person-fill"></i> Nombre del Usuario:</h5></td>
-                <td><?php echo $data['name_user'];?></td>
-            </tr>
-            <tr>
-                <td><h5><i class="bi bi-envelope-fill"></i> Email:</h5></td>
-                <td><?php echo $data['email'];?></td>
-            </tr>
-            <tr>
-                <td><h5><i class="bi bi-key-fill"></i> Password:</h5></td>
-                <td><?php echo $data['passwd'];?></td>
-            </tr>
-            <tr>
-                <td><h5><i class="bi bi-phone-fill"></i> Phone:</h5></td>
-                <td><?php echo $data['phone'];?></td>
-            </tr>
-            <?php 
+                <tr>
+                    <td class="col-5"><h5><i class="bi bi-qr-code"></i> Id De La Andadera:</h5></td>
+                    <td class="col-5"><?php echo $data['id_walker'];?></td>
+                </tr>
+                <tr>
+                    <td><h5><i class="bi bi-person-fill"></i> Nombre del Usuario:</h5></td>
+                    <td><?php echo $data['name_user'];?></td>
+                </tr>
+                <tr>
+                    <td><h5><i class="bi bi-envelope-fill"></i> Email:</h5></td>
+                    <td><?php echo $data['email'];?></td>
+                </tr>
+                <tr>
+                    <td><h5><i class="bi bi-key-fill"></i> Password:</h5></td>
+                    <td><p type="password"><?php echo $data['passwd'];?></p></td>
+                </tr>
+                <tr>
+                    <td><h5><i class="bi bi-phone-fill"></i> Phone:</h5></td>
+                    <td><?php echo $data['phone'];?></td>
+                </tr>
+                <?php 
 		}
 		?>
         </tbody>
